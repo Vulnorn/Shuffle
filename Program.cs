be3f8f1ea2fa1aff29d58f157b0f31d34e2b2647
Vulnorn <mixadrbk31@gmail.com>
2023-06-26 15:22:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Xml.Linq;
+
+namespace DataBasePlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeArray = 10;
+            int maxElementArray = 100;
+
+            Random random = new Random();
+
+            int[] array = new int[sizeArray];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(maxElementArray);
+            }
+
+            Console.WriteLine("Массив до перемешивания");
+            ShowArray(array);
+            Shuffle(array, random);
+
+            Console.WriteLine($"\nМассив после перемешивания");
+            ShowArray(array);
+
+            Console.ReadKey();
+        }
+
+        static void Shuffle(int[] array, Random random)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomIndex = i;
+
+                while (randomIndex == i)
+                {
+                    randomIndex = random.Next(array.Length);
+                }
+
+                int elementArray = array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i] = elementArray;
+            }
+        }
+
+        static void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]} ");
+        }
+    }
+}
